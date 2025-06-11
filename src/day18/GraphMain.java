3ffe5d7989d9383e25a97a3dda81399ca8ec2f50
Karthick Murugesan G <karthickmg2022@gmail.com>
2025-06-11 09:36:42 +0530

    Update GraphMain.java

--- src/day18/GraphMain.java
+++ src/day18/GraphMain.java
@@ -6,10 +6,15 @@ public class GraphMain {
 	ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 	for (int i = 0; i < V; i++)
 		adj.add(new ArrayList<>());
-	adj.get(0).add(1); adj.get(0).add(2); adj.get(0).add(3);
-	adj.get(1).add(0); adj.get(1).add(4);
-	adj.get(2).add(0); adj.get(2).add(3);
-	adj.get(3).add(0); adj.get(3).add(2);
+	adj.get(0).add(1); 
+	adj.get(0).add(2); 
+	adj.get(0).add(3);
+	adj.get(1).add(0); 
+	adj.get(1).add(4);
+	adj.get(2).add(0);
+	adj.get(2).add(3);
+	adj.get(3).add(0); 
+	adj.get(3).add(2);
 	adj.get(4).add(1);
 	BFS bfs = new BFS();
 	System.out.println("BFS Traversal: " + bfs.bfsOfGraph(V, adj));
