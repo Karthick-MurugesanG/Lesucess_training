51c7d7e58206fff6ddc54eb68b61d6b0c4ad2381
Karthick Murugesan G <karthickmg2022@gmail.com>
2025-06-12 09:59:36 +0530

    Update Anagrams.groovy

--- GroovyAssignment/src/day19/Anagrams.groovy
+++ GroovyAssignment/src/day19/Anagrams.groovy
@@ -1,6 +1,5 @@
 package day19
 
-def Anagrams(String str1, String str2) {
- return str1.toList().sort() == str2.toList().sort()
-}
-println Anagrams("listen", "silent") 
+str1="listen"
+str2="silent"
+println str1.toList().sort() == str2.toList().sort()
